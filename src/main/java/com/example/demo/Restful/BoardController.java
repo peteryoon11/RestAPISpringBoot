982e827861e4b3aae09a6a680296a6b73b430fc8
peteryoon11 <yjisub11@gmail.com>
2018-11-24 22:55:45 +0900

    Repository 추가

--- src/main/java/com/example/demo/Restful/BoardController.java
+++ src/main/java/com/example/demo/Restful/BoardController.java
@@ -3,9 +3,23 @@ package com.example.demo.Restful;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.Restful.DTO.NoteRepository;
+import com.example.demo.Restful.DTO.BoardRepository;
 
 public class BoardController{
 	@Autowired
 	private NoteRepository noteRepository;
-	private BoardReposirty boardRepositroy;
+	private BoardRepository boardRepositroy;
+	public NoteRepository getNoteRepository() {
+		return noteRepository;
+	}
+	public void setNoteRepository(NoteRepository noteRepository) {
+		this.noteRepository = noteRepository;
+	}
+	public BoardRepository getBoardRepositroy() {
+		return boardRepositroy;
+	}
+	public void setBoardRepositroy(BoardRepository boardRepositroy) {
+		this.boardRepositroy = boardRepositroy;
+	}
+	
 }
---
+++ src/main/java/com/example/demo/Restful/DTO/BoardRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.Restful.DTO;
+
+public class BoardRepository {
+
+}
